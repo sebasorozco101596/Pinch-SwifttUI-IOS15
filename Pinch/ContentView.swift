4ee730bb4ed7ab09b66f54418d3c83249d08d65c
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-16 23:26:11 -0500

    Initial Commit

---
+++ Pinch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
