2e50b5828e28a62d5c416c0dbc7adb5abddb7c87
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-17 00:22:23 -0500

    Adding the drag gesture for to explore the image in detail. Also I added animations for to see the image smooth when the image is coming to the original size

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -13,17 +13,24 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     //MARK: - FUNCTION
     
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
     //MARK: - CONTENT
-    
-    
+
     var body: some View {
         
         NavigationView {
-            
             ZStack {
+                //MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -31,6 +38,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 //MARK: - 1. TAP GESTURE
@@ -40,11 +48,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
