0a9a62024e8354d1f873aaaaf5c68b10a011c0cb
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-18 01:46:28 -0500

    Adding the thumbNails and doing the final touches for to have the app complete

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/18/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
     @State private var imageOffset: CGSize = .zero
     @State private var isDrawerOpen: Bool = false
     
+    let pages: [Page] = pagesData
+    @State private var pageIndex: Int = 1
+    
     //MARK: - FUNCTION
     
     func resetImageState() {
@@ -23,6 +26,10 @@ struct ContentView: View {
             imageScale = 1
             imageOffset = .zero
         }
+    }
+    
+    func currentPage() -> String {
+        return pages[pageIndex - 1].imageName
     }
     
     //MARK: - CONTENT
@@ -35,7 +42,7 @@ struct ContentView: View {
                 Color.clear
                 
                 //MARK: - PAGE IMAGE
-                Image("magazine-front-cover")
+                Image(currentPage())
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
@@ -174,6 +181,22 @@ struct ContentView: View {
                         }
                     
                     //MARK: - THUMBNAILS
+                    
+                    ForEach(pages) { item in
+                        Image(item.thumbnailName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                            .cornerRadius(8)
+                            .shadow(radius: 4)
+                            .opacity(isDrawerOpen ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isDrawerOpen)
+                            .onTapGesture {
+                                isAnimating = true
+                                pageIndex = item.id
+                            }
+                    }
+                    
                     Spacer()
                 }//: DRAWER
                     .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
